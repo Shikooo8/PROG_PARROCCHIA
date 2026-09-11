07a5ad906831559f02a035ca2ac42d644a7bee41
Shikooo8 <scifalcioli@gmail.com>
2026-09-11 12:30:16 +0200

    Create project

---
+++ sito_backend/src/main/java/it/parrocchiatrasfigurazione/sito_backend/SitoBackendApplication.java
@@ -0,0 +1,13 @@
+package it.parrocchiatrasfigurazione.sito_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitoBackendApplication.class, args);
+	}
+
+}
---
+++ sito_backend/src/test/java/it/parrocchiatrasfigurazione/sito_backend/SitoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.parrocchiatrasfigurazione.sito_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
